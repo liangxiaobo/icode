3b363057dcd18265fca594822c0715bfae5b71b8
liangxiaobo <liangxiaobo2009@yeah.net>
2016-04-13 11:44:38 +0800

    去掉详情页中的按钮

--- views/ccontent/view.php
+++ views/ccontent/view.php
@@ -38,16 +38,6 @@ $this->registerJsFile('@web/script/code_block.js',['depends'=>['yii\web\JqueryAs
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
 <?php echo $model->content; ?>
     <!-- DetailView::widget([
         'model' => $model,
